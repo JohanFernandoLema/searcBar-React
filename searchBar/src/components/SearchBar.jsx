608ba02dcd23984230b70d17183b57ffe9bfaf0b
JohanFernandoLema <johan.13fernando@gmail.com>
2023-04-09 13:08:16 -0400

    sfiles skeleton complete

---
+++ searchBar/src/components/SearchBar.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+import React from "react";
+
+import { FaSearch } from "react-icons/fa"
+
+import "./SearchBar.css"
+
+export const SearchBar = () =>{
+
+    const [input, setInput] = useState("");
+
+    const fetchData = (value) => {
+        fetch("https://jsonplaceholder.typicode.com/users").then((response) => response.json()).then((json) => {
+            const results = json.filter((user) => {
+                return user && user.name && user.name.toLowerCase().includes(value);
+            })
+        
+         console.log(results);
+        });
+    }
+
+    const handleChange = (value) => {
+        setInput(value);
+        fetchData(value);
+    }
+
+    return(
+        <div className="input-wrapper">
+            <FaSearch id="search-icon"/>
+            <input placeholder="Type to search..." value={input} onChange={(e) => handleChange(e.target.value)}/>
+        </div>
+    );
+};
+
